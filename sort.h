6cc7fe78d03b8e62488874d2c4486ed41b5c4527
Abu1914 <dmuktaru@gmail.com>
2024-01-24 07:17:35 +0000

    Learning sorting algorithms

--- sort.h
+++ sort.h
@@ -29,6 +29,6 @@ void insert_swap(listint_t **h, listint_t *first_node, listint_t *second_node);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int l_partition(int *array, int low, int high, size_t size);
-
+void cocktail_sort_list(listint_t **list);
 
 #endif
